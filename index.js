e98f83c260ddfdbd8247f4a3801c0b21e9a54a87
bherth <bherthchavez@gmail.com>
2022-08-01 10:15:25 +0800

    Todo List v2 with database

--- index.js
+++ index.js
@@ -2,39 +2,87 @@
 const express = require("express");
 const https = require("https")
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-const items = ["Buy Food","Cook Food", "Eat Food"];
-const workItems = [];
-
-
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
+const itemsSchema = {
+name: String
+};
+
+const Item = mongoose.model("item", itemsSchema);
+
+const item1 = new Item({
+name: "Welcome to your todolist!",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item",
+  });
+
+  const item3 = new Item({
+    name: "<--Hit this to delete an item.>!",
+    });
+    
+const defaultItems = [item1, item2, item3];
 
 
  app.get("/", function(req, res){
-   const day = date.getDate();
+  //  const day = date.getDate();
+
+  Item.find({},function(err, foundItems){
+    if (foundItems.length === 0 ){
+        Item.insertMany(defaultItems, function(err){
+          if (err) {
+            console.log(err);
+          }else{
+            console.log("Successfully saved default item to DB.");
+          }
+        });
+        res.redirect("/");
+    }else{
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
     
-    res.render("list", {listTitle: day, newListItem: items});
- });
-
-
+  });
+  });
+    
+ 
  app.post("/", function(req, res){
-   const item = req.body.newItem;
+   const newItem = req.body.newItem;
 
     if(req.body.list === "Work"){
-      workItems.push(item)
+      const item = new Item({
+        name: newItem
+        });
+        item.save();
       res.redirect("/work");
     }else{
-      items.push(item);
+      const item = new Item({
+        name: newItem
+        });
+        item.save();
       res.redirect("/");
     }
     
  });
+
+
+app.post("/delete", function(req, res){
+const checkedItemId = req.body.checkbox;
+Item.findByIdAndRemove(checkedItemId, function(err){
+  if (!err){
+    console.log("Successfully deleted checked item.");
+    res.redirect("/");
+  }
+});
+
+});
 
 
  app.get("/work", function(req, res){
